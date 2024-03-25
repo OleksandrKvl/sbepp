d9418a762171b490729e85e3c7adf0d30171cb7b
OleksandrKvl <OleksandrKvl@users.noreply.github.com>
2024-03-25 11:54:15 +0000

    deploy: b7a3e323bb4c99301b6faf58a18efc0d7d0f0b98

--- search/all_7.js
+++ search/all_7.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['has_5fvalue_0',['has_value',['../classsbepp_1_1detail_1_1optional__base.html#ad2a0d48a5f8448e41a65838af24a4269',1,'sbepp::detail::optional_base']]],
-  ['header_5ftype_1',['header_type',['../classsbepp_1_1schema__traits.html#a9a53b31c788e848ac09e89fa4d2bf7c0',1,'sbepp::schema_traits']]],
-  ['header_5ftype_5ftag_2',['header_type_tag',['../classsbepp_1_1schema__traits.html#a2d45acd52784e4ec7bc89f440095bc76',1,'sbepp::schema_traits']]],
-  ['helpers_3',['CMake helpers',['../integration.html#autotoc_md6',1,'']]],
-  ['how_20it_20compares_20to_20reallogic_4',['How it compares to RealLogic',['../index.html#autotoc_md1',1,'']]],
-  ['how_20it_20works_5',['How it works',['../index.html#autotoc_md0',1,'']]]
+  ['header_1',['Decoding message header',['../examples.html#autotoc_md42',1,'']]],
+  ['header_5ftype_2',['header_type',['../classsbepp_1_1schema__traits.html#a9a53b31c788e848ac09e89fa4d2bf7c0',1,'sbepp::schema_traits']]],
+  ['header_5ftype_5ftag_3',['header_type_tag',['../classsbepp_1_1schema__traits.html#a2d45acd52784e4ec7bc89f440095bc76',1,'sbepp::schema_traits']]],
+  ['helpers_4',['CMake helpers',['../integration.html#autotoc_md6',1,'']]],
+  ['how_20it_20compares_20to_20reallogic_5',['How it compares to RealLogic',['../index.html#autotoc_md1',1,'']]],
+  ['how_20it_20works_6',['How it works',['../index.html#autotoc_md0',1,'']]]
 ];
