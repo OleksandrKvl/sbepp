124d250f1fe4b4d20206c8639f011d57aba9bae2
OleksandrKvl <OleksandrKvl@users.noreply.github.com>
2024-04-04 16:22:30 +0000

    deploy: bf15a1f7f1d6a53e16f27faa12e19d147c76e0f1

--- search/all_10.js
+++ search/all_10.js
@@ -2,11 +2,12 @@ var searchData=
 [
   ['tags_0',['Tags',['../traits.html#tags',1,'']]],
   ['the_20schema_20compiler_1',['Using the schema compiler',['../sbeppc.html',1,'']]],
-  ['to_20reallogic_2',['How it compares to RealLogic',['../index.html#autotoc_md1',1,'']]],
-  ['to_5funderlying_3',['to_underlying',['../namespacesbepp.html#a0d0a0af58987f30ee37fda64c15cc316',1,'sbepp']]],
-  ['traits_4',['Traits',['../traits.html',1,'Traits'],['../traits.html#autotoc_md32',1,'Using traits']]],
-  ['traits_20list_5',['Traits list',['../group__traits-list.html',1,'']]],
-  ['type_6',['type',['../structsbepp_1_1byte__type.html#a5309c044561c3ad72f35fa55f6d61ed7',1,'sbepp::byte_type']]],
-  ['type_5ftraits_7',['type_traits',['../classsbepp_1_1type__traits.html',1,'sbepp']]],
-  ['types_8',['Optional/required types',['../representation.html#non-array-types',1,'']]]
+  ['to_20encode_20a_20message_2',['Estimating buffer size to encode a message',['../examples.html#example-size-bytes',1,'']]],
+  ['to_20reallogic_3',['How it compares to RealLogic',['../index.html#autotoc_md1',1,'']]],
+  ['to_5funderlying_4',['to_underlying',['../namespacesbepp.html#a0d0a0af58987f30ee37fda64c15cc316',1,'sbepp']]],
+  ['traits_5',['Traits',['../traits.html',1,'Traits'],['../traits.html#autotoc_md32',1,'Using traits']]],
+  ['traits_20list_6',['Traits list',['../group__traits-list.html',1,'']]],
+  ['type_7',['type',['../structsbepp_1_1byte__type.html#a5309c044561c3ad72f35fa55f6d61ed7',1,'sbepp::byte_type']]],
+  ['type_5ftraits_8',['type_traits',['../classsbepp_1_1type__traits.html',1,'sbepp']]],
+  ['types_9',['Optional/required types',['../representation.html#non-array-types',1,'']]]
 ];
