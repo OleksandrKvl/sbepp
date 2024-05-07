99655d5ae8b7ed9437b373bde46b5801c6f43700
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2024-05-07 11:37:19 +0300

    take GH Pages repo name into account

--- doc/version_selector_handler.js
+++ doc/version_selector_handler.js
@@ -2,19 +2,19 @@
 // Copyright (c) 2024, Oleksandr Koval
 
 $(function () {
-    $.get('/version_selector.html', function (data) {
+    var repoName = window.location.pathname.split('/')[1];
+    $.get('/' + repoName + '/version_selector.html', function (data) {
         // Inject version selector HTML into the page
         $('#projectnumber').html(data);
 
         // Event listener to handle version selection
         document.getElementById('versionSelector').addEventListener('change', function () {
             var selectedVersion = this.value;
-            window.location.href = '/' + selectedVersion + '/index.html';
-
+            window.location.href = '/' + repoName + '/' + selectedVersion + '/index.html';
         });
 
         // Set the selected option based on the current version
-        var currentVersion = window.location.pathname.split('/')[1];
+        var currentVersion = window.location.pathname.split('/')[2];
         $('#versionSelector').val(currentVersion);
     });
 });
