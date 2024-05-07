29125de407c21b07bfd79e900b3da650de88d12b
OleksandrKvl <OleksandrKvl@users.noreply.github.com>
2024-05-07 08:19:28 +0000

    deploy: b8bcd509e5a14d2fc40545705e8478f35df7c1e8

---
+++ git-main/classsbepp_1_1schema__traits.js
@@ -0,0 +1,5 @@
+var classsbepp_1_1schema__traits =
+[
+    [ "header_type", "classsbepp_1_1schema__traits.html#a9a53b31c788e848ac09e89fa4d2bf7c0", null ],
+    [ "header_type_tag", "classsbepp_1_1schema__traits.html#a2d45acd52784e4ec7bc89f440095bc76", null ]
+];
---
+++ git-main/search/all_6.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['get_5fheader_0',['get_header',['../namespacesbepp.html#a12bbc67580e3f4bb2a0299033f01818d',1,'sbepp']]],
+  ['group_20entries_1',['Group entries',['../representation.html#autotoc_md26',1,'']]],
+  ['group_5ftraits_2',['group_traits',['../classsbepp_1_1group__traits.html',1,'sbepp']]],
+  ['groups_3',['Groups',['../representation.html#groups',1,'']]]
+];
