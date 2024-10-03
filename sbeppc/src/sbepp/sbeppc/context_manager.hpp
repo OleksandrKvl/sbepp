986a99e6297d2f70414fe66195d28f8c97e56fa3
Oleksandr Koval <oleksandr.koval.dev@gmail.com>
2024-10-03 20:27:58 +0300

    use `context_manager` in `tags_generator`

--- sbeppc/src/sbepp/sbeppc/context_manager.hpp
+++ sbeppc/src/sbepp/sbeppc/context_manager.hpp
@@ -16,34 +16,35 @@ namespace sbepp::sbeppc
 // TODO: extract context types into separate headers?
 struct type_context
 {
-    // std::string tag;
-    // std::string impl_name;
-    // std::string impl_type;
-    // std::string underlying_type;
     std::size_t size;
-    // std::string public_type;
-    // bool is_template;
     // here and below, set only if encoding is located inside a composite
     std::optional<offset_t> offset_in_composite;
+    // std::string public_type;
+    // bool is_template;
+    std::string tag;
+    std::string impl_name;
+    // std::string impl_type;
+    // std::string underlying_type;
 };
 
 struct composite_context
 {
-    // std::string tag;
-    // std::string impl_name;
-    // std::string impl_type;
     std::size_t size;
-    // std::string public_type;
     std::optional<offset_t> offset_in_composite;
+
+    std::string tag;
+    std::string impl_name;
+    // std::string impl_type;
+    // std::string public_type;
 };
 
 struct ref_context
 {
     std::size_t size;
-    // std::string tag;
     // unlike other encodings, ref is always within a composite so the member
     // is not optional
     offset_t offset_in_composite;
+    std::string tag;
 };
 
 struct enum_valid_value_context
@@ -53,13 +54,13 @@ struct enum_valid_value_context
 
 struct enumeration_context
 {
-    // std::string tag;
-    // std::string impl_name;
-    // std::string impl_type;
-    // std::string underlying_type;
     std::size_t size;
     // std::string public_type;
     std::optional<offset_t> offset_in_composite;
+    std::string tag;
+    std::string impl_name;
+    // std::string impl_type;
+    // std::string underlying_type;
 };
 
 struct set_choice_context
@@ -69,32 +70,31 @@ struct set_choice_context
 
 struct set_context
 {
-    // TODO: seems this context is similar to enum's one
-    // std::string tag;
-    // std::string impl_name;
-    // std::string impl_type;
-    // std::string underlying_type;
     std::size_t size;
     // std::string public_type;
     std::optional<offset_t> offset_in_composite;
+    std::string tag;
+    std::string impl_name;
+    // std::string impl_type;
+    // std::string underlying_type;
 };
 
 struct message_context
 {
     std::string tag;
     std::string impl_name;
-    std::string impl_type;
-    std::string public_type;
-    block_length_t actual_block_length;
+    // std::string impl_type;
+    // std::string public_type;
+    // block_length_t actual_block_length;
 };
 
 struct field_context
 {
-    // std::string tag;
     // does NOT include message/group header size
     offset_t level_offset;
     std::size_t size;
     field_presence actual_presence;
+    std::string tag;
     // std::string value_type;
     // std::string value_type_tag;
     // bool is_template;
@@ -103,23 +103,23 @@ struct field_context
 struct group_context
 {
     std::string tag;
-    std::string entry_impl_type;
     std::string impl_name;
-    std::string impl_type;
-    block_length_t actual_block_length;
+    // std::string entry_impl_type;
+    // std::string impl_type;
+    // block_length_t actual_block_length;
 };
 
 struct data_context
 {
     std::string tag;
-    const sbe::type* length_type;
-    std::string impl_type;
+    // const sbe::type* length_type;
+    // std::string impl_type;
 };
 
 struct message_schema_context
 {
     std::string name;
-    // std::string tag;
+    std::string tag;
 };
 
 template<typename T>
@@ -170,6 +170,36 @@ struct context_type<sbe::message_schema>
     using type = message_schema_context;
 };
 
+template<>
+struct context_type<sbe::enum_valid_value>
+{
+    using type = enum_valid_value_context;
+};
+
+template<>
+struct context_type<sbe::set_choice>
+{
+    using type = set_choice_context;
+};
+
+template<>
+struct context_type<sbe::group>
+{
+    using type = group_context;
+};
+
+template<>
+struct context_type<sbe::data>
+{
+    using type = data_context;
+};
+
+template<>
+struct context_type<sbe::message>
+{
+    using type = message_context;
+};
+
 class context_manager
 
 {
@@ -178,6 +208,12 @@ public:
     context_type_t<T>& get(const T& t)
     {
         return std::get<map_type<T>>(contexts)[&t];
+    }
+
+    template<typename T>
+    const context_type_t<T>& get(const T& t) const
+    {
+        return std::get<map_type<T>>(contexts).at(&t);
     }
 
 private:
@@ -191,7 +227,12 @@ private:
         map_type<sbe::composite>,
         map_type<sbe::ref>,
         map_type<sbe::field>,
-        map_type<sbe::message_schema>>
+        map_type<sbe::message_schema>,
+        map_type<sbe::enum_valid_value>,
+        map_type<sbe::set_choice>,
+        map_type<sbe::group>,
+        map_type<sbe::data>,
+        map_type<sbe::message>>
         contexts;
 };
 } // namespace sbepp::sbeppc
